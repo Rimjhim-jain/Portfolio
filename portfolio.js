e636578446ef1f1bfdc0bcec4b2fd94b0946a69a
Rimjhim Jain <rimjhimsmile16@gmail.com>
2022-04-13 01:14:42 +0530

    portfolio

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
 
     $('#menu').click(function(){
@@ -42,48 +41,3 @@ $(document).ready(function(){
 
 });
 
-=======
-$(document).ready(function(){
-
-    $('#menu').click(function(){
-      $(this).toggleClass('fa-times');
-      $('header').toggleClass('toggle');
-    });
-
-    //scroll up
-    $(window).on('scroll load',function(){
-
-        $('#menu').removeClass('fa-times');
-        $('header').removeClass('toggle');
-    
-        if($(window).scrollTop() > 0){
-          $('.top').show();
-        }else{
-          $('.top').hide();
-        }
-    
-      });
-    
-      // smooth scrolling 
-    
-      $('a[href*="#"]').on('click',function(e){
-    
-        e.preventDefault();
-    
-        $('html, body').animate({
-    
-          scrollTop : $($(this).attr('href')).offset().top,
-    
-        },
-          500, 
-          'linear'
-        );
-    
-      });
-    
-      
-
-
-});
-
->>>>>>> b223fa2176ee2c360aeeb0f0d54181ad3759c2b8
